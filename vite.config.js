20856f767a5ceb23dee1ee4f1ebd3f9e1ae14d9a
srinivasmeda-brs <svas77310@gmail.com>
2024-12-23 14:46:38 +0530

    frontend

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: process.env.PORT || 4173, // Use the PORT environment variable or fall back to 5173
-    host: true, // Allow external access
+    port: parseInt(process.env.PORT) || 5173, // Use Render's port or fallback
+    host: true,
   },
   plugins: [react()],
 })
