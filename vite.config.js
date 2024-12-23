44674c6bc4be249a24317ab0fd22815106ccf5c6
srinivasmeda-brs <svas77310@gmail.com>
2024-12-23 14:36:44 +0530

    stoybook1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 4173, // Use the PORT environment variable or fall back to 5173
+    host: true, // Allow external access
+  },
   plugins: [react()],
 })
