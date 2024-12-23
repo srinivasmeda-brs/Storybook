697baf949c8803a043023d36f4d8395a917f92ba
srinivasmeda-brs <svas77310@gmail.com>
2024-12-23 15:15:21 +0530

    frontend

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/register',
   server: {
     port: parseInt(process.env.PORT) || 5173,
     host: true,
