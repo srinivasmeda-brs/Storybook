be16c52fd7bda1cd6997bada500aaef5e68f7f9b
srinivasmeda-brs <svas77310@gmail.com>
2024-12-24 10:03:09 +0530

    commit1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // This allows the app to be accessible externally (useful for testing on different devices)
+    port: process.env.PORT || 5173, // You can replace this with your desired port number
+  },
+});
